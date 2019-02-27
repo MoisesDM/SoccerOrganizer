febc1e9ad3535a854fcca8783a347a8cf0d468dc
hp <moy99dm@gmail.com>
2019-02-27 14:02:32 -0600

    le di accion al boton de menu

--- app/src/main/java/com/soccer_organizer/soccerorganizer/MainActivity.java
+++ app/src/main/java/com/soccer_organizer/soccerorganizer/MainActivity.java
@@ -1,13 +1,26 @@
 package com.soccer_organizer.soccerorganizer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+this.menu=findViewById(R.id.btnMenu);
+this.menu.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplication(),ArbritosActivity.class);
+    startActivity(intent);
+    }
+
+});
+
     }
 }
