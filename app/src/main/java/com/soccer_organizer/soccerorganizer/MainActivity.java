041030ce81295fb2d61a6f8726a19778b6ad8adb
hp <moy99dm@gmail.com>
2019-03-06 10:53:48 -0600

    hola

--- app/src/main/java/com/soccer_organizer/soccerorganizer/MainActivity.java
+++ app/src/main/java/com/soccer_organizer/soccerorganizer/MainActivity.java
@@ -7,20 +7,21 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-private Button menu;
+    private Button menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-this.menu=findViewById(R.id.btnMenu);
-this.menu.setOnClickListener(new View.OnClickListener() {
-    @Override
-    public void onClick(View v) {
-        Intent intent = new Intent(getApplication(),ArbritosActivity.class);
-    startActivity(intent);
-    }
+        this.menu = findViewById(R.id.btnMenu);
+        this.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplication(), ArbritosActivity.class);
+                startActivity(intent);
+            }
 
-});
+        });
 
     }
 }
